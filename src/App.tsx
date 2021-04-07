b3117c242c7dd3748212b09abda1ab7865e5a882
Estyn Edwards <estyn@punchcard.io>
2021-04-06 23:38:42 -0600

    local cache

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,52 @@ import MembershipCard from './membership-card';
 import { MembershipProps } from './membership';
 
 function App() {
-  var m: MembershipProps = {
+
+  var members: Array<MembershipProps> = [{
     name: 'Jack Edwards',
     grandLodgeNumber: '63044',
     lodgeName: 'Avon Glen',
     lodgeNumber: '170',
     duesExpireDate: 'Dec 31, 2020',
     attestedBy: 'Louis Fradette'
-  };
+  }, {
+    name: 'Fred Flinstone',
+    grandLodgeNumber: '63044',
+    lodgeName: ' Loyal Order of Water Buffalo',
+    lodgeNumber: '170',
+    duesExpireDate: 'Dec 31, 2020',
+    attestedBy: 'Barney Rubble'
+  }];
+  let m: MembershipProps | undefined;
+  //check local storage first
+  const localMember = localStorage.getItem('member');
+  //if found, then use it
+  if (localMember !== null)
+    m = JSON.parse(localMember) as MembershipProps;
+  else {
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const queryId = params.get('id');
+    if (queryId === 'secret') {
+      m = members[0];
+    }
+    else if (queryId == 'flintStones')
+      m = members[1];
+  }
+  //if not check url
+  if (m !== undefined) {
+    localStorage.setItem('member', JSON.stringify(m));
+  }
+  //make "api" call
+
+  //store in storage
+
+
+
   return (
     <div className="App">
-      <MembershipCard {...m} />
+      {m !== undefined ?
+        <MembershipCard {...m} /> : <p>Nope</p>}
     </div>
   );
 }
